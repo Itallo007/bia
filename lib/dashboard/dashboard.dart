260c49babf2269fadfdfae601be32c7840e29f7f
Itallo007 <italosantos.oficial2000@gmail.com>
2022-05-26 00:35:52 -0300

    Creates the bottomAppBar

--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatefulWidget {
@@ -14,10 +13,75 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Dashboard")),
-      body: Center(
-        child: Text(widget.message)
+      appBar: AppBar(
+        title: Text("Dashboard"),
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.exit_to_app),
+              onPressed: () {
+                Navigator.of(context).pushReplacementNamed('/');
+              })
+        ],
       ),
+      body: Center(child: Text(widget.message)),
+      bottomNavigationBar: BottomAppBar(
+          color: Colors.green,
+          child: IconTheme(
+            data: IconThemeData(color: Colors.white),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      tooltip: 'Inicio',
+                      icon: Icon(Icons.home),
+                      onPressed: () {},
+                    ),
+                    Text('Inicio', style: TextStyle(color: Colors.white),)
+                  ],
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      tooltip: 'Vagas',
+                      icon: Icon(Icons.work),
+                      onPressed: () {},
+                    ),
+                    Text('Vagas', style: TextStyle(color: Colors.white),)
+                  ],
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      tooltip: 'Alunos',
+                      icon: Icon(Icons.group),
+                      onPressed: () {},
+                    ),
+                    Text('Alunos', style: TextStyle(color: Colors.white),)
+                  ],
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      tooltip: 'Mais',
+                      icon: Icon(Icons.menu),
+                      onPressed: () {},
+                    ),
+                    Text('Mais', style: TextStyle(color: Colors.white),)
+                  ],
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
