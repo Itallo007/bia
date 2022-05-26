4bd837c29d99c8a1bbdb4d4cae53103ca036dd35
Itallo007 <italosantos.oficial2000@gmail.com>
2022-05-25 22:56:07 -0300

    Creates the Curso and Modalidade models

---
+++ lib/curso/model/curso.dart
@@ -0,0 +1,20 @@
+import 'package:bia/modalidade_curso/model/modalidade_curso.dart';
+import 'package:bia/professor/model/professor.dart';
+
+class Curso {
+  final String _nome;
+  final String _turno;
+  final ModalidadeCurso _modalidadeCurso;
+  final Professor _coordenador;
+
+  Curso(this._nome, this._turno, this._modalidadeCurso, this._coordenador);
+
+  String get nome => _nome;
+
+  String get turno => _turno;
+
+  ModalidadeCurso get modalidadeCurso => _modalidadeCurso;
+
+  Professor get coordenador => _coordenador;
+
+}
---
+++ lib/modalidade_curso/model/modalidade_curso.dart
@@ -0,0 +1,7 @@
+class ModalidadeCurso {
+  final String _nome;
+
+  ModalidadeCurso(this._nome);
+
+  String get nome => _nome;
+}
