54cc2d8ea8273d092d6a685d7f48d11b0a9003fc
Beatriz-Borges <beatrizsobsantana@gmail.com>
2022-06-23 20:46:14 -0300

    tela de login flutter

--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -8,11 +8,10 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
-
   int _selectedIndex = 0;
 
   List<Widget> screens = [
-    Text('Inicio'),
+    Text('Início'),
     Text('Vagas'),
     Text('Alunos'),
     Text('Mais'),
@@ -27,46 +26,31 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Dashboard"),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.exit_to_app),
-            onPressed: () {
-              Navigator.of(context).pushReplacementNamed('/');
-            })
-        ],
-      ),
-      body: Center(child: screens.elementAt(_selectedIndex)),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Inicio',
-            backgroundColor: Colors.green
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.work),
-            label: 'Vagas',
-            backgroundColor: Colors.green
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.group),
-            label: 'Alunos',
-            backgroundColor: Colors.green
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.menu),
-            label: 'Mais',
-            backgroundColor: Colors.green
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.white60,
-        showUnselectedLabels: true,
-        onTap: onTapItem,
-      )
-    );
+        appBar: AppBar(
+          title: Text("BIA - Basic Intership Administrator"),
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 18, 73, 41),
+          actions: [
+            IconButton(
+                icon: const Icon(Icons.exit_to_app),
+                onPressed: () {
+                  Navigator.of(context).pushReplacementNamed('/');
+                })
+          ],
+        ),
+        body: Center(child: screens.elementAt(_selectedIndex)),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Início', backgroundColor: Color.fromARGB(255, 18, 73, 41)),
+            BottomNavigationBarItem(icon: Icon(Icons.work), label: 'Vagas', backgroundColor: Color.fromARGB(255, 18, 73, 41)),
+            BottomNavigationBarItem(icon: Icon(Icons.group), label: 'Alunos', backgroundColor: Color.fromARGB(255, 18, 73, 41)),
+            BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'Mais', backgroundColor: Color.fromARGB(255, 18, 73, 41)),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white60,
+          showUnselectedLabels: true,
+          onTap: onTapItem,
+        ));
   }
 }
--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,25 +1,106 @@
-
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
-  const Login({Key? key, }) : super(key: key);
+  const Login({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Material(// cria uma tela simples
-      child: Column (// coloca uma coluna dentro da tela
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [// lista de elementos da coluna
-          Text('Welcome to BIA'),
-          ElevatedButton(// cria um botão
-            onPressed: () {
-              //navega para a rota '/dashboard' quando o botão é clicado
-              Navigator.of(context).pushReplacementNamed('/dashboard');
-            },
-            child: Text('Entrar')
-          )
-        ],
-      )
+    return Scaffold(
+      body: Container(
+        color: Color.fromARGB(150, 210, 233, 183),
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+        ),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 300,
+              height: 135,
+              child: Image.asset("imagens/bialogo.png"),
+            ),
+            SizedBox(
+              height: 80,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                fillColor: Color.fromARGB(148, 154, 194, 150),
+                filled: true,
+                border: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Color.fromARGB(148, 154, 194, 150), width: 2.0),
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                labelText: "E-mail",
+                labelStyle: TextStyle(
+                  color: Color.fromARGB(117, 117, 116, 116),
+                  fontWeight: FontWeight.w400,
+                  fontSize: 18,
+                ),
+              ),
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              decoration: InputDecoration(
+                  fillColor: Color.fromARGB(148, 154, 194, 150),
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Color.fromARGB(148, 154, 194, 150), width: 2.0),
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  labelText: "Senha",
+                  labelStyle: TextStyle(
+                    color: Color.fromARGB(117, 117, 116, 116),
+                    fontWeight: FontWeight.w400,
+                    fontSize: 18,
+                  )),
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, left: 90, right: 90),
+              height: 50,
+              width: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(30),
+                ),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.2, 1],
+                  colors: [
+                    Color.fromARGB(148, 154, 194, 150),
+                    Color.fromARGB(255, 18, 73, 41),
+                  ],
+                ),
+              ),
+              child: TextButton(
+                child: Center(
+                    child: Text(
+                  'ENTRAR',
+                  style: TextStyle(color: Color.fromARGB(218, 235, 229, 229)),
+                )),
+                onPressed: () {
+                  //navega para a rota '/dashboard' quando o botão é clicado
+                  Navigator.of(context).pushReplacementNamed('/dashboard');
+                },
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
