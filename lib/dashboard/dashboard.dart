2fc8940ffdefa692c53d4d3ec47a820bb3f38c3a
Beatriz-Borges <beatrizsobsantana@gmail.com>
2022-06-24 13:51:06 -0300

    Tela de cadastrar alunos (substitua o codigo no login)

--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:bia/aluno/view/cadastro.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -12,7 +11,55 @@ class _DashboardState extends State<Dashboard> {
   int _selectedIndex = 0;
 
   List<Widget> screens = [
-    Text('Início'),
+    Container(
+      color: Color.fromARGB(150, 210, 233, 183),
+      padding: EdgeInsets.only(
+        top: 60,
+        left: 40,
+        right: 40,
+      ),
+      child: ListView(
+        children: <Widget>[
+          SizedBox(
+            width: 200,
+            height: 86,
+            child: Image.asset("imagens/bia_logo.png"),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10, left: 5, right: 170),
+            height: 150,
+            width: 170,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(
+                Radius.circular(30),
+              ),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: [0.2, 1],
+                colors: [
+                  Color.fromARGB(148, 154, 194, 150),
+                  Color.fromARGB(255, 18, 73, 41),
+                ],
+              ),
+            ),
+            child: TextButton(
+              child: Center(
+                  child: Text(
+                '',
+                style: TextStyle(color: Color.fromARGB(218, 235, 229, 229)),
+              )),
+              onPressed: () {
+                //navega para a rota '/dashboard' quando o botão é clicado
+              },
+            ),
+          ),
+        ],
+      ),
+    ),
     Container(
       color: Color.fromARGB(150, 210, 233, 183),
       padding: EdgeInsets.only(
@@ -118,9 +165,7 @@ class _DashboardState extends State<Dashboard> {
                 'Atualizar dados de uma vaga',
                 style: TextStyle(color: Color.fromARGB(218, 235, 229, 229)),
               )),
-              onPressed: () {
-                //navega para a rota '/dashboard' quando o botão é clicado
-              },
+              onPressed: () {},
             ),
           ),
           SizedBox(
@@ -349,7 +394,7 @@ class _DashboardState extends State<Dashboard> {
     return Scaffold(
         appBar: AppBar(
           title: Text("BIA - Basic Internship Administrator"),
-          centerTitle: true,
+          centerTitle: false,
           backgroundColor: Color.fromARGB(255, 18, 73, 41),
           actions: [
             IconButton(
