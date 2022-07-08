d13a9b77dd814cfaf626638e98e53225b0ce2d20
Itallo007 <italosantos.oficial2000@gmail.com>
2022-07-08 01:02:25 -0300

    Atualiza a tela de cadastro de vaga

--- lib/vagas/view/cadastrar.dart
+++ lib/vagas/view/cadastrar.dart
@@ -1,45 +1,40 @@
 import 'package:flutter/material.dart';
 
-class CadastrarVagas extends StatelessWidget {
-  const CadastrarVagas({
-    Key? key,
-  }) : super(key: key);
+class CadastrarVagas extends StatefulWidget {
+  const CadastrarVagas({Key? key}) : super(key: key);
+  
+  @override
+  State<CadastrarVagas> createState() => new _CadastrarVagasState();
+}
+
+class _CadastrarVagasState extends State<CadastrarVagas> {
+
+  final organizationList = <String>['MegaNews', 'Prefeitura', 'Soft Com', 'Grupo exito'];
+  String dropDownOrganizationValue = '';
+
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pushReplacementNamed('/dashboard');
+          },
+        ),
         title: Text("Cadastrar uma vaga"),
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 18, 73, 41),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.of(context).pushReplacementNamed('/dashboard');
-            },
-          ),
-        ],
       ),
       body: Container(
         color: Color.fromARGB(150, 210, 233, 183),
-        padding: EdgeInsets.only(
-          top: 60,
-          left: 40,
-          right: 40,
-        ),
+        padding: EdgeInsets.symmetric(horizontal: 32),
         child: ListView(
           children: <Widget>[
             SizedBox(
-              height: 0,
-            ),
-            SizedBox(
-              width: 200,
-              height: 86,
-              child: Image.asset("imagens/bia_logo.png"),
-            ),
-            SizedBox(
-              height: 40,
+              height: 32,
             ),
             TextFormField(
               keyboardType: TextInputType.text,
@@ -56,69 +51,62 @@ class CadastrarVagas extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: 30,
+              height: 32,
             ),
-            SizedBox(
-              child: Text("Descrição - CONSERTARRRRRRRRRRRRRRRRR"),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.fromLTRB(0, 40, 0, 40),
-                fillColor: Color.fromARGB(251, 255, 255, 255),
-                filled: true,
-                border: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Color.fromARGB(148, 154, 194, 150), width: 5.0),
-                  borderRadius: BorderRadius.circular(10.0),
+            ConstrainedBox(
+              constraints: BoxConstraints(maxHeight: 150),
+              child: TextFormField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: "Descrição",
+                  hintText: "Digite a descrição da vaga",
+                  hintStyle: TextStyle(
+                    color: Color.fromARGB(117, 117, 116, 116),
+                  ),
+                  contentPadding: EdgeInsets.all(8),
+                  fillColor: Color.fromARGB(251, 255, 255, 255),
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                        color: Color.fromARGB(148, 154, 194, 150), width: 5.0),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
                 ),
+                maxLines: null,
               ),
             ),
-            TextFormField(
-              keyboardType: TextInputType.text,
+            DropdownButtonFormField<String> (
+              hint: Text('Selecione a empresa'),
+              value: (dropDownOrganizationValue.isEmpty ? null: dropDownOrganizationValue),
+              onChanged: (newValue) => {
+                setState(() => {
+                  dropDownOrganizationValue = newValue!
+                })
+              },
+              items: organizationList.map((String e) => DropdownMenuItem(
+                value: e,
+                child: Text(e)
+              )).toList(),
               decoration: InputDecoration(
-                labelText: "Unidade concedente - OPÇÃO DE ROLAR",
+                labelText: "Concedente",
                 labelStyle: TextStyle(
                   color: Color.fromARGB(117, 117, 116, 116),
                   fontWeight: FontWeight.w400,
                   fontSize: 18,
                 ),
-              ),
-              style: TextStyle(
-                fontSize: 20,
-              ),
+              )
             ),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                labelText: "Remunerado - CHECKBOX",
-                labelStyle: TextStyle(
-                  color: Color.fromARGB(117, 117, 116, 116),
-                  fontWeight: FontWeight.w400,
-                  fontSize: 18,
-                ),
-              ),
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            TextFormField(
-              //precisa alterar curso para checkbox
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Status - OPÇÃO DE ROLAR",
-                labelStyle: TextStyle(
-                  color: Color.fromARGB(117, 117, 116, 116),
-                  fontWeight: FontWeight.w400,
-                  fontSize: 18,
-                ),
-              ),
-              style: TextStyle(
-                fontSize: 20,
-              ),
+            Row(
+              children: [
+                Checkbox(
+                  value: isChecked,
+                  onChanged: (bool? newValue) => {
+                    setState(() => {
+                      isChecked = newValue!
+                    })
+                }),
+                Text('Remunerado')
+              ],
             ),
             SizedBox(
               height: 40,
